32fb38b14e5654ff258c796e5e47422ce14346ad
Darknet <darknet@Mac.Home>
2026-03-30 21:47:58 +0500

    Fix database connection

--- database.py
+++ database.py
@@ -1,14 +1,14 @@
+cat > ~/dating-site/database.py << 'EOF'
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from config import DATABASE_URL
 
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-from config import DATABASE_URL
+if DATABASE_URL.startswith("sqlite"):
+    engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+else:
+    engine = create_engine(DATABASE_URL)
 
-engine = create_engine(DATABASE_URL, connect_args={"client_encoding": "utf8"})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
@@ -19,3 +19,4 @@ def get_db():
         yield db
     finally:
         db.close()
+EOF
